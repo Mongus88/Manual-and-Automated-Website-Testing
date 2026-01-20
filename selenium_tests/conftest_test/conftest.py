8ec95362ea6fbd627e0a297b33f557fe4196510d
Iványi Tamás <ivatam88@gmail.com>
2026-01-20 20:39:23 +0100

    Update conftest.py

--- selenium_tests/conftest_test/conftest.py
+++ selenium_tests/conftest_test/conftest.py
@@ -1,1 +1,16 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+@pytest.fixture
+def driver():
+    options = Options()
+    options.add_experimental_option("detach", True)
+
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+
+    yield driver
+
+    driver.quit()
